cba1b486ade23ec4605825d142add3eeecf673c7
Efesto Gomora <efestogomora395@gmail.com>
2026-05-27 12:16:57 +0200

    Update spar_app.py

--- spar_app.py
+++ spar_app.py
@@ -33,7 +33,7 @@ ADMIN_EMAIL = "gomoraefesto97@gmail.com"
 # ============================================
 # WEBHOOK URL (Updated with your new tunnel)
 # ============================================
-WEBHOOK_URL = "https://cayman-attended-issn-simpson.trycloudflare.com "
+WEBHOOK_URL = "https://cayman-attended-issn-simpson.trycloudflare.com"
 
 # ============================================
 # SPAR BRAND COLORS
